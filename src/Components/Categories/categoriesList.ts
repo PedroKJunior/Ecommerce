af29d2c37e9fa738e91fba73e3bc9f2548ce239b
Pedro Junior <juniorkart@hotmail.com>
2022-02-17 23:58:09 -0300

    Cria a lista com os objetos de categoria

---
+++ src/Components/Categories/categoriesList.ts
@@ -0,0 +1,28 @@
+interface CategoriesItens {
+	_id: number
+	img: string
+	text: string
+}
+
+export const categoriesList: CategoriesItens[] = [
+	{
+		_id: 0,
+		text: 'Roupas Femininas',
+		img: 'https://i0.wp.com/lojapkd.com.br/wp-content/uploads/2021/12/roupa-feminina-moda-pkd-varejo-confortavel-conjunto-estilosa-jeans-site-201.jpg?fit=771%2C1080&ssl=1',
+	},
+	{
+		_id: 1,
+		text: 'Roupas Masculinas',
+		img: 'https://i.pinimg.com/564x/48/92/4d/48924da959fadbb0608dd292a6e9a533.jpg',
+	},
+	{
+		_id: 2,
+		text: 'Joias',
+		img: 'https://img.elo7.com.br/product/original/35ED33D/kit-4-pulseiras-feminina-folheada-a-ouro-semi-joias-kit-4-pulseiras-feminina-folheada-a-ouro-semi-jo.jpg',
+	},
+	{
+		_id: 3,
+		text: 'Eletrônicos',
+		img: 'https://www.lojasolar.com.br/custom/content/themes/lojassolar/imagens/SEO/eletronicos-1.jpg',
+	},
+]
