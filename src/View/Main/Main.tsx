f5977d85e8f3bfb1fec8c52e99cca4ffc9ff8e3f
Pedro Junior <juniorkart@hotmail.com>
2022-02-24 01:39:06 -0300

    Add Select, metódo de sorted e seleção de category

--- src/View/Main/Main.tsx
+++ src/View/Main/Main.tsx
@@ -1,38 +1,76 @@
 import './main.sass'
-import { useEffect, useState } from 'react'
+import React, { useEffect, useState } from 'react'
 
 import { getListProducts } from '../../API/products'
 import Categories from '../../Components/Categories/Categories'
 import ProductCard from '../../Components/PoductCard/ProductCard'
+import Select from '../../Components/Select/Select'
+import { ProductOrder } from '../../Constants/ProductOrder'
 import { Product } from '../../Interfaces/IProducts'
+import { lowestPrice, biggestPrice, bestRated } from '../../Shared/sortedMethod'
 
 const Main = () => {
-	const [products, setProducts] = useState<Product[]>()
+	const [products, setProducts] = useState<Product[]>([])
+	const [category, setCategory] = useState<string>('todos')
 
 	useEffect(() => {
-		const fetchProducts = async () => {
-			const response = await getListProducts()
+		const fetchProducts = async (category: string) => {
+			const response = await getListProducts(category)
 			setProducts(response)
 		}
 
-		fetchProducts()
-	}, [])
+		fetchProducts(category)
+	}, [category])
+
+	const handleClickCategory = (event: React.MouseEvent<HTMLDivElement>) => {
+		setCategory(String(event.currentTarget.dataset.category))
+	}
+
+	const handleAllProducts = () => {
+		if (category !== 'todos') setCategory('todos')
+	}
+
+	const selectOrder = (value: number) => {
+		const methodSortedProducts = (a: Product, b: Product) => {
+			if (value === 3) return bestRated(a, b)
+			if (value === 2) return biggestPrice(a, b)
+			return lowestPrice(a, b)
+		}
+
+		const clonedProducts = [...products]
+		const sortedProducts = clonedProducts?.sort((a, b) =>
+			methodSortedProducts(a, b)
+		)
+		setProducts(sortedProducts)
+	}
 
 	return (
 		<div className="content-main">
-			<div className="categories">
-				<Categories />
-				<ul>
-					<li>Produtos</li>
-					<li>/</li>
-					<li> Roupa Feminina </li>
-				</ul>
+			<div className="header">
+				<div className="categories">
+					<Categories onClick={handleClickCategory} />
+					<ul>
+						<li
+							onClick={handleAllProducts}
+							className={`${category !== 'todos' && 'active'}`}
+						>
+							Produtos
+						</li>
+						<li>/</li>
+						<li> {category} </li>
+					</ul>
+				</div>
+				<Select
+					valuesList={ProductOrder}
+					initialValue={'Filtrar por'}
+					selectValue={selectOrder}
+				/>
 			</div>
 
 			<div className="list-products">
-				{products?.map((product) => (
-					<ProductCard product={product} key={product.id} />
-				))}
+				{products.map((product) => {
+					return <ProductCard product={product} key={product.id} />
+				})}
 			</div>
 		</div>
 	)
