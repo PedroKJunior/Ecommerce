4b9a2dfcaf8ace841d1a82f489d430a57278be8d
Pedro Junior <juniorkart@hotmail.com>
2022-02-24 01:32:03 -0300

    Adiciona método de click e data-type ao card

--- src/Components/CategoryCard/CategoryCard.tsx
+++ src/Components/CategoryCard/CategoryCard.tsx
@@ -3,11 +3,13 @@ import './categoryCard.sass'
 interface PropsCategoryCard {
 	image: string
 	text: string
+	type: string
+	onClick(event: React.MouseEvent<HTMLDivElement>): void
 }
 
-const CategoryCard = ({ image, text }: PropsCategoryCard) => {
+const CategoryCard = ({ image, text, type, onClick }: PropsCategoryCard) => {
 	return (
-		<div className="category-card">
+		<div className="category-card" onClick={onClick} data-category={type}>
 			<div
 				className="image"
 				style={{ backgroundImage: `url(${image})` }}
