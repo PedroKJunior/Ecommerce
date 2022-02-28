78d9ceaacdf4d9b54c73aa150b4aecc3b6c84017
Pedro Junior <juniorkart@hotmail.com>
2022-02-28 01:08:23 -0300

    Cria o Context para o Cart

---
+++ src/Store/CartContext.tsx
@@ -0,0 +1,38 @@
+import { createContext, ReactNode, useContext, useState } from 'react'
+
+import { Product } from '../Interfaces/IProducts'
+
+interface IProviderContext {
+	children: ReactNode
+}
+
+interface ICart {
+	products?: Array<Product>
+	add?(product: Product): void
+	remove?(id: number): void
+}
+
+const CartContext = createContext<ICart>({})
+
+export const CartContextProvider = ({ children }: IProviderContext) => {
+	const [products, setProducts] = useState<Product[]>([])
+
+	const add = (product: Product) => {
+		const arrayAux = products
+		arrayAux.push(product)
+		setProducts(arrayAux)
+	}
+
+	const remove = (id: number) => {
+		const arrayAux = products.filter((items) => items.id !== id)
+		setProducts(arrayAux)
+	}
+
+	return (
+		<CartContext.Provider value={{ products, add, remove }}>
+			{children}
+		</CartContext.Provider>
+	)
+}
+
+export const cartContext = useContext(CartContext)
