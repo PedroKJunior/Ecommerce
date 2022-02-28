427bd860a872493f4d7a11d64363fe2924d6bca4
Pedro Junior <juniorkart@hotmail.com>
2022-02-28 01:08:34 -0300

    Add o context na app

--- src/index.tsx
+++ src/index.tsx
@@ -2,10 +2,13 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 
 import App from './App'
+import { CartContextProvider } from './Store/CartContext'
 
 ReactDOM.render(
 	<React.StrictMode>
-		<App />
+		<CartContextProvider>
+			<App />
+		</CartContextProvider>
 	</React.StrictMode>,
 	document.getElementById('root')
 )
