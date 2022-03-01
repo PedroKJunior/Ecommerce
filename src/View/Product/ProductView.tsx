d7d1c2c8ef4281f8238ff90b5ee1fec2cc093b8d
Pedro Junior <juniorkart@hotmail.com>
2022-03-01 13:29:33 -0300

    Add Loading

--- src/View/Product/ProductView.tsx
+++ src/View/Product/ProductView.tsx
@@ -3,6 +3,7 @@ import { RiShoppingCartFill } from 'react-icons/ri'
 import { useParams } from 'react-router-dom'
 
 import { getOneProduct } from '../../API/products'
+import Loading from '../../Components/Loading/Loading'
 import BarRating from '../../Components/Rating/BarRating'
 import { Product } from '../../Interfaces/IProducts'
 import { cartContext } from '../../Store/CartContext'
@@ -27,31 +28,41 @@ const ProductView = () => {
 	}
 
 	return (
-		<div className="content-product-view">
-			<div
-				className="content-image"
-				style={{ backgroundImage: `url(${product?.image})` }}
-			></div>
-			<div className="container-informations">
-				<div className="content-informations">
-					<h3>{product?.title}</h3>
-					<div className="rating">
-						<span>{product?.rating.rate}</span>
-						<BarRating value={product?.rating.rate || 0} />
-						<div className="separator"></div>
-						<span>{product?.rating.count} Avaliações</span>
-					</div>
-					<div className="price">U$ {product?.price}</div>
-					<div className="description">{product?.description} </div>
-					<div className="content-button">
-						<button onClick={handleClick}>
-							<RiShoppingCartFill size={20} />
-							Adicionar ao Carrinho
-						</button>
+		<>
+			{product ? (
+				<div className="content-product-view">
+					<div
+						className="content-image"
+						style={{ backgroundImage: `url(${product?.image})` }}
+					></div>
+					<div className="container-informations">
+						<div className="content-informations">
+							<h3>{product?.title}</h3>
+							<div className="rating">
+								<span>{product?.rating.rate}</span>
+								<BarRating value={product?.rating.rate || 0} />
+								<div className="separator"></div>
+								<span>{product?.rating.count} Avaliações</span>
+							</div>
+							<div className="price">U$ {product?.price}</div>
+							<div className="description">
+								{product?.description}{' '}
+							</div>
+							<div className="content-button">
+								<button onClick={handleClick}>
+									<RiShoppingCartFill size={20} />
+									Adicionar ao Carrinho
+								</button>
+							</div>
+						</div>
 					</div>
 				</div>
-			</div>
-		</div>
+			) : (
+				<div className="loading">
+					<Loading color="#2553e9" />
+				</div>
+			)}
+		</>
 	)
 }
 export default ProductView
