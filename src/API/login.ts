c393a167b0d56b5d554897387105120e93f9ca40
Pedro Junior <juniorkart@hotmail.com>
2022-03-01 15:57:53 -0300

    Cria um Login Fake

--- src/API/login.ts
+++ src/API/login.ts
@@ -1,15 +1,14 @@
 import { IUser } from '../Interfaces/IUser'
-import { api } from './general'
 
 export const userLogin = async (user: IUser): Promise<any> => {
-	try {
-		const { data } = await api.post('/auth/login', JSON.stringify({
-			username: user.user,
-			password: user.password
-		}))
-
-		return data
-	} catch (err) {
-		return 'error'
-	}
+	return new Promise((resolve) =>
+		setTimeout((i = 0) => {
+			if (user.user === 'living_teste' && user.password === 'living') {
+				localStorage.setItem('auth', JSON.stringify(true))
+				resolve(true)
+			} else {
+				resolve('error')
+			}
+		}, 5000)
+	)
 }
