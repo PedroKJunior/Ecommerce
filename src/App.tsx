8308435789905c655e882318f1dfc21830e0fa5e
Pedro Junior <juniorkart@hotmail.com>
2022-03-01 15:57:37 -0300

    Cria Rota entre Login e demais paginas

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,12 @@
 import { BrowserRouter } from 'react-router-dom'
 
-import AppRoutes from './Routes/Routes'
+import { LoginRoutes } from './Routes/Routes'
 import './Styles/global.sass'
-
-//import MainMenu from './Components/MainMenu/MainMenu'
 
 function App() {
 	return (
 		<BrowserRouter>
-			{/* <MainMenu /> */}
-			{/* <Loading color="#0433FF" /> */}
-			<AppRoutes />
+			<LoginRoutes />
 		</BrowserRouter>
 	)
 }
