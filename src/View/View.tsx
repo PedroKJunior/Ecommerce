f19e7ae0173e4e27cbc8c2e030139f06a590f790
Pedro Junior <juniorkart@hotmail.com>
2022-03-01 15:59:26 -0300

    Cria a tela para as subrotas

---
+++ src/View/View.tsx
@@ -0,0 +1,12 @@
+import MainMenu from '../Components/MainMenu/MainMenu'
+import { AppRoutes } from '../Routes/Routes'
+
+const View = () => {
+	return (
+		<>
+			<MainMenu />
+			<AppRoutes />
+		</>
+	)
+}
+export default View
